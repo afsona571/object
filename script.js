af599dd097bc7a0a678be024eff034e00f6e4fe9
afsona571 <afsonaa59@gmail.com>
2024-11-20 18:44:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let userName = prompt("Введите ваше имя")
+let id = prompt("Введите ваш id из space")
+let parol = prompt("Введите ваш  пароль из space")
+
+;
+
+ let obj = { name: userName , id: id, parol: parol,}
+ 
+ 
+ console.log(obj);
+// if (userName.includes(letters)) {
+//     alert("в вашем имени присутствует  буква " + letter + " !")
+// }
+
+
+
